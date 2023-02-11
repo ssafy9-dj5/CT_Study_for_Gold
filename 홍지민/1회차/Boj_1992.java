2905d0bc86db49a79cc1fbf5b699d5e6f582fd1c
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2023-02-11 21:23:42 +0900

    Update Boj_1992.java

--- 홍지민/1회차/Boj_1992.java
+++ 홍지민/1회차/Boj_1992.java
@@ -8,24 +8,24 @@ public class Boj_1992 {
     static int[][] map;
     static StringBuilder sb;
     public static void zone(int r, int c, int div){
-        if(check(r,c,div)){
+        if(check(r,c,div)){ // 특정 구역의 모든 숫자가 같은지 검사 후 숫자 추가
             if(map[r][c] == 1) sb.append(1);
             else sb.append(0);
             return;
         }
 
         int n_div = div/2;
-        sb.append("(");
+        sb.append("("); // 구역 나누기 시작할 때 ( 추가
         
-        zone(r,c,n_div);
-        zone(r,c+n_div, n_div);
-        zone(r+n_div, c, n_div);
-        zone(r+n_div, c+n_div, n_div);
-        sb.append(")");
+        zone(r,c,n_div); // 2사분면
+        zone(r,c+n_div, n_div); // 1사분면
+        zone(r+n_div, c, n_div); // 3사분면 
+        zone(r+n_div, c+n_div, n_div); // 4사분면
+        sb.append(")"); // 모든 구역 탐색 후 ) 추가
     }
 
     public static boolean check(int r, int c, int div){
-        int first = map[r][c];
+        int first = map[r][c]; // 왼쪽 위를 기준으로 지정
 
         for(int i=r; i<r+div; i++){
             for(int j=c; j<c+div; j++){
