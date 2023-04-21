1a2d5cd031a732d14bc5e0cb5beff1c3e2b1207d
hzim0422 <hzim0422@gmail.com>
2023-04-21 22:13:43 +0900

    홍지민 10회차 풀이제출

---
+++ 홍지민/10회차/programmers_가장가까운글자.java
@@ -0,0 +1,27 @@
+package day0423;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        boolean[] exist = new boolean[26];
+        
+        for(int i=0; i<s.length(); i++){
+            int num = s.charAt(i)-97;
+            if(!exist[num]){
+                answer[i] = -1;
+                exist[num] = true;
+            }
+            else {
+                int idx = 1;
+                for(int j=i-1; j>=0; j--){
+                    if(s.charAt(j) == s.charAt(i)){
+                        answer[i] = idx;
+                        break;
+                    }
+                    idx++;
+                }
+            }
+        }
+        return answer;
+    }
+}
